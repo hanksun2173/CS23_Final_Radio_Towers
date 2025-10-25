a70a55d9b73fd49be5237e5cb07dd52aef65a1cd
ggiann02 <giorgianna26@gmail.com>
2025-10-25 18:19:41 -0400

    Added basic platformer functionality

---
+++ CS23_Radio_Towers/Assets/Scripts/PlayerPlatformMovement.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class PlayerPlatformMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    public int extraJumpsValue = 2;
+
+    private Rigidbody2D rb;
+    private int extraJumps;
+
+    private bool isGrounded;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();    
+        extraJumps = extraJumpsValue;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (isGrounded) {
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (isGrounded) {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            }
+            else if (extraJumps > 0) {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                extraJumps--;
+
+            }
+        }
+        
+    }
+
+    private void FixedUpdate() {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+}
