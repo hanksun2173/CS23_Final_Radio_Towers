0d978392626166c6e42a7cdde479ca37065551f2
ggiann02 <giorgianna26@gmail.com>
2025-10-27 13:55:38 -0400

    Ladder functionality

---
+++ CS23_Radio_Towers/Assets/Scripts/LadderMovement.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class LadderMovement : MonoBehaviour
+{
+
+    private float vertical;
+    private float speed = 8f;
+    private bool isLadder;
+    private bool isClimbing;
+
+    [SerializeField] private Rigidbody2D rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        vertical = Input.GetAxis("Vertical");
+
+        if (isLadder && Mathf.Abs(vertical) > 0f)
+        {
+            isClimbing = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClimbing)
+        {
+            rb.gravityScale = 0f;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, vertical * speed);   
+        } else
+        {
+            rb.gravityScale = 1f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = true;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ladder"))
+        {
+            isLadder = false;
+            isClimbing = false;
+        }
+    }
+}
