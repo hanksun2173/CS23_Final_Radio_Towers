51c7495d71e7831bb2ee3bdfad8904b46e3ce89a
Bird2525 <116972901+Bird2525@users.noreply.github.com>
2025-11-11 17:40:20 -0500

    Minigame
    Minigame WIP

---
+++ CS23_Radio_Towers/Assets/PuzzleClicker/PuzzleConnector.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class PuzzleConnector : MonoBehaviour
+{
+	public bool isConnectedToPower = false;
+
+    void Start(){
+    }
+
+	//requires a Rigidbody2d to register collisins. Set gravity = 0:
+    void OnTriggerStay2D(Collider2D other){
+
+        if (other.gameObject.tag=="Connector"){
+			if (other.gameObject.GetComponentInParent<PuzzleClicker>().isPowered){
+				isConnectedToPower = true;
+				
+				if (GetComponentInParent<PuzzleClicker>().isEnd == true){
+					GetComponentInParent<PuzzleMASTER>().PuzzleComplete();
+					GetComponentInParent<PuzzleClicker>().EndYellow();
+				}
+				if (GetComponentInParent<PuzzleClicker>().isBomb == true){
+					//add a consquence. Boom, lose? Or are wetrying to detnate the bombs?
+					GetComponentInParent<PuzzleClicker>().EndRed();
+					Debug.Log("You powered a bomb!");
+				}
+			}
+			else{
+				isConnectedToPower = false;
+			}
+			//Debug.Log("I am connected to power!");
+		} 
+    }
+
+	void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag=="Connector"){
+			isConnectedToPower = false; // [problematic logic?]
+		} 
+    }
+}
--- CS23_Radio_Towers/Assets/Scripts/PipeRotate.cs
+++ CS23_Radio_Towers/Assets/Scripts/PipeRotate.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PipeRotate : MonoBehaviour
 {
@@ -6,6 +7,14 @@ public class PipeRotate : MonoBehaviour
     Vector3 rotation = new Vector3(0,0,90);
 
     public void Rotate(){
+        Debug.Log("hi");
         pipe.Rotate(rotation);
     }
+
+
+    public void OnPointerClick(PointerEventData eventData){
+        Debug.Log("hi");
+        pipe.Rotate(rotation);
+    }
+
 }
