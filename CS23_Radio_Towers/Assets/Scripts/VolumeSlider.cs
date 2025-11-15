46bf69181bea6b0e3d56a7d9121e947485a6072e
ssanyush <shayna.ssanyu@tufts.edu>
2025-11-15 16:53:15 -0500

    added volume bttn to pause menu

---
+++ CS23_Radio_Towers/Assets/Scripts/VolumeSlider.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("mainvolume", Mathf.Log10(volume)*20);
+    }
+}
