b69830aec6300e5b3778474f35fd7b1459b66bde
ssanyush <shayna.ssanyu@tufts.edu>
2025-11-17 21:55:29 -0500

    credits menu UI

---
+++ CS23_Radio_Towers/Assets/Scripts/Credits.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Credits : MonoBehaviour
+{
+    public float scrollSpeed = 40f;
+
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+              rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update()
+    {
+        rectTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+    }
+}
