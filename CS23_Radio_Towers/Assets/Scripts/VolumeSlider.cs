6f32bf0fe8919af79dc925ad53880485ff9475e5
ggiann02 <giorgianna26@gmail.com>
2025-11-18 19:13:28 -0500

    Volume and friction fix

--- CS23_Radio_Towers/Assets/Scripts/VolumeSlider.cs
+++ CS23_Radio_Towers/Assets/Scripts/VolumeSlider.cs
@@ -7,9 +7,11 @@ public class VolumeSlider : MonoBehaviour
     [SerializeField] private AudioMixer myMixer;
     [SerializeField] private Slider musicSlider;
 
+
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
         myMixer.SetFloat("mainvolume", Mathf.Log10(volume)*20);
     }
+    
 }
