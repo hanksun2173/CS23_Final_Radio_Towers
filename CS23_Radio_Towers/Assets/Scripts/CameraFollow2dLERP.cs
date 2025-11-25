04c73f872ec3ceeb7e1b5bf7db26cfe8bb592394
ggiann02 <giorgianna26@gmail.com>
2025-11-25 16:44:45 -0500

    minor fix

--- CS23_Radio_Towers/Assets/Scripts/CameraFollow2dLERP.cs
+++ CS23_Radio_Towers/Assets/Scripts/CameraFollow2dLERP.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using UnityEngine;
 
 public class CameraFollow2dLERP : MonoBehaviour {
+      public Vector3 offset = new Vector3(0, 2f, 0); // 2 units higher
 
       private GameObject target;
       public float camSpeed = 4.0f;
@@ -12,7 +13,8 @@ public class CameraFollow2dLERP : MonoBehaviour {
       }
 
       void FixedUpdate () {
-            Vector2 pos = Vector2.Lerp ((Vector2)transform.position, (Vector2)target.transform.position, camSpeed * Time.fixedDeltaTime);
-            transform.position = new Vector3 (pos.x, pos.y, transform.position.z);
+            Vector3 targetPos = target.transform.position + offset;
+            Vector2 pos = Vector2.Lerp((Vector2)transform.position, (Vector2)targetPos, camSpeed * Time.fixedDeltaTime);
+            transform.position = new Vector3(pos.x, pos.y, transform.position.z);
       }
 }
