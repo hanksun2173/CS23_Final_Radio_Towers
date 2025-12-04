f1cf1340a4f6340144eae041b85693ab73efcba4
jennifercmay <jennifer.may@tufts.edu>
2025-12-04 00:52:20 -0500

    cloud script

---
+++ CS23_Radio_Towers/Assets/Scripts/Cloud.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float speed = 0.5f;
+    private Vector2 destination;
+    public int distance = 2500; 
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        destination = new Vector2(transform.position.x + 2500, transform.position.y);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       Vector2 pos = Vector2.Lerp((Vector2)transform.position, destination, speed * Time.fixedDeltaTime);
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+        
+        float distancetoEnd = vector3.Distance(transform.position, destination);    
+        if (distancetoEnd <= 5)
+        {
+            Destroy(gambject);
+        }
+    }
+}
---
+++ CS23_Radio_Towers/Assets/Scripts/CloudManager.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+public class CloudManager : MonoBehaviour
+{
+
+    public GameObject[] clouds;
+      private GameObject target;
+      public float camSpeed = 4.0f;
+
+      void Start(){
+            target = GameObject.FindWithTag("Player");
+      }
+
+      void FixedUpdate () {
+            Vector3 targetPos = target.transform.position + offset;
+            Vector2 pos = Vector2.Lerp((Vector2)transform.position, (Vector2)targetPos, camSpeed * Time.fixedDeltaTime);
+            transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+      }
+}
+
